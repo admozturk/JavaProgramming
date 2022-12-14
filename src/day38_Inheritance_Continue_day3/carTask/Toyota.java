48d9b646c17bf0bd7b7a7c3c1fee4037ffdbc867
Adem OZTURK <admoztrk86@gmail.com>
2022-12-14 11:32:23 +0100

    Toyota class is now completed

---
+++ src/day38_Inheritance_Continue_day3/carTask/Toyota.java
@@ -0,0 +1,21 @@
+package day38_Inheritance_Continue_day3.carTask;
+
+public class Toyota extends Car{
+
+    public Toyota(String brand, String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
+    }
+
+    public void reliable (){
+        System.out.println(brand+model+" is reliable");
+    }
+
+
+
+
+}
+/*
+toyota:
+extra methods:
+           reliable()
+           */
